ec53cf2758e02fee0491c6a8090e009fe531bc8d
PiyushBose <piyushbose05@gmail.com>
2024-11-20 22:54:30 +0530

    Basic layout done

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 import { quizData } from "./data.js";
 
-console.log(quizData[1].a);
+function render(i){
+    const curr = quizData[i];
+    document.querySelector(".question").innerHTML = curr.question;
+    document.querySelector("#option-1-text").innerHTML = curr.a;
+    document.querySelector("#option-2-text").innerHTML = curr.b;
+    document.querySelector("#option-3-text").innerHTML = curr.c;
+    document.querySelector("#option-4-text").innerHTML = curr.d;
+}
+
+let idx = 0;
+
+render(idx);
+
+const btn = document.querySelector(".submit");
+
+btn.addEventListener("click", () => {
+    if(idx < quizData.length - 1) 
+        render(++ idx);
+    else if(idx === quizData.length - 1){
+        idx ++;
+        loadEndScreen();
+    }
+    else reload();
+});
+
+function loadEndScreen(){
+    console.log("end");
+}
+
+function reload(){
+    render(0);
+    idx = 0;
+    console.log("reload");
+}
